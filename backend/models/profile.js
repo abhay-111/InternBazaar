d404fcf296ab109825cce284c689829d6319baae
namans511 <srivastava.naman511@gmail.com>
2020-09-28 14:48:34 +0530

    updated user profile

--- backend/models/profile.js
+++ backend/models/profile.js
@@ -2,7 +2,30 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const educationSchema = require("./education-profile");
+// education object
+const education = {
+  collegeName: {
+    type: String,
+    required: true,
+  },
+  degree: {
+    type: String,
+    required: true,
+  },
+  startYear: {
+    type: Number,
+  },
+  endYear: {
+    type: Number,
+    required: true,
+  },
+  performanceScale: {
+    type: String,
+  },
+  performance: {
+    type: Decimal128,
+  },
+};
 
 // defining the schema model for User resgistration
 const profileSchema = new Schema({
@@ -10,15 +33,15 @@ const profileSchema = new Schema({
     type: String,
     required: true,
   },
-  //   password: {
-  //     type: String,
-  //     required: true,
-  //   },
   collegeName: {
     type: String,
     required: true,
   },
-  education: [educationSchema],
+  education: [education],
+  phone: String,
+  location: [String],
+  skills: [String],
+  links: [String],
 });
 
 module.exports = mongoose.model("profile", profileSchema);
