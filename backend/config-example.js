52d59150df69fa8fbce7d332498b4273d2d11f90
namans511 <srivastava.naman511@gmail.com>
2020-10-05 01:23:38 +0530

    fixed login bug

--- backend/config-example.js
+++ backend/config-example.js
@@ -1,6 +1,10 @@
-var config = {
-  mongoapikey: "uvw",
-  mailgunapikey: "abc",
-  mailgundomain: "xyz",
-  apikey: "ijk",
+const config = {
+  apikey: "abcd",
+  mongoapikey:
+    "mongodb+srv://<username>:<password>@cluster0.vnmzf.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  mailgunapikey: "xyz",
+  mailgundomain: "abc.mailgun.org",
+  tokenkey: "your-token-key",
 };
+
+module.exports = config;
